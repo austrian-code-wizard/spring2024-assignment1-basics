b588cea284a1d0e507d9dc7706c49cad95dc19b5
Moritz Stephan <moritz.stephan@stanford.edu>
2024-04-16 23:52:51 -0700

    fixing trainer

--- cs336_basics/loss.py
+++ cs336_basics/loss.py
@@ -2,12 +2,8 @@ import torch
 
 
 def cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-    # Logits: B x S x V
-    # Targets: B x S
-    print(logits.shape)
-    print(targets.shape)
     logits -= logits.max(-1, keepdim=True)[0]
-    loss = -logits[torch.arange(logits.shape[0]), torch.arange(logits.shape[1]), targets].squeeze() + torch.log(
+    nnl = -logits.gather(-1, targets.unsqueeze(1)).squeeze() + torch.log(
         torch.exp(logits).sum(-1)
     )
-    return loss.mean()
+    return nnl.mean()
